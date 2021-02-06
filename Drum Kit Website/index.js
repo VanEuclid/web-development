962ffcf9360a3eab68f927852b712331c1693eb9
Van Euclid <68357572+VanEuclid@users.noreply.github.com>
2021-02-05 18:03:06 -0700

    Update

--- Drum Kit Website/index.js
+++ Drum Kit Website/index.js
@@ -1,6 +1,58 @@
 var drumButtons = document.querySelectorAll(".drum").length;
-for(var i = 0; i < drumButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got clicked!");
+
+for (var i = 0; i < drumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var drum = this.innerHTML;
+    makeSound(drum);
+    buttonAnimation(drum);
   });
+}
+
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function buttonAnimation(key) {
+  var activeButton = document.querySelector("." + key);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    default:
+      console.log();
+  }
 }
