09dbf9d58f1b3d04811acded87019bf198548c19
VanEuclid <vaneuclidd@gmail.com>
2021-03-31 18:39:49 -0600

    Adding React

---
+++ cryptocurrency-api/src/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+const axios = require('axios');
+
+app.get("/bitcoin", (req, res) => {
+    axios.get('https://api.coingecko.com/api/v3/coins/bitcoin?tickers=true&market_data=true&community_data=false&developer_data=false&sparkline=false')
+        .then(response => {
+            console.log(response.data.market_data.current_price.usd);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+
+app.get("/cardano", (req, res) => {
+    axios.get('https://api.coingecko.com/api/v3/coins/cardano?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false')
+        .then(response => {
+            console.log(response.data.market_data.current_price.usd);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+
+app.listen(3000, () => {
+    console.log("server running on port 3000");
+});
